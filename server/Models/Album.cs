cea34fa8b7e4c0a30145239d274f0f87e51764e9
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-17 09:53:44 -0600

    album model

---
+++ server/Models/Album.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace postit_dotnet.Models;
+
+public class Album
+{
+  public int Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  [MinLength(3), MaxLength(25)]
+  public string Title { get; set; }
+  [MinLength(15), MaxLength(250)]
+  public string Description { get; set; }
+  [MaxLength(2000)]
+  public string CoverImg { get; set; }
+  public bool Archived { get; set; }
+  public string Category { get; set; }
+  public string CreatorId { get; set; }
+}
