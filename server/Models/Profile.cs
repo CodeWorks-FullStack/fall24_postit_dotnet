8b0a95cd23245397278f576a422fc792059fe4ed
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-18 10:03:29 -0600

    added timestamps to profile

--- server/Models/Profile.cs
+++ server/Models/Profile.cs
@@ -4,6 +4,8 @@ namespace postit_dotnet.Models;
 public class Profile
 {
   public string Id { get; set; }
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
   public string Name { get; set; }
   public string Picture { get; set; }
 }
