65bac7831b39012411b985fb99ef77f7bb0012dc
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-18 09:30:14 -0600

    created watcher files

---
+++ server/Repositories/WatchersRepository.cs
@@ -0,0 +1,15 @@
+
+
+
+
+namespace postit_dotnet.Repositories;
+
+public class WatchersRepository
+{
+  public WatchersRepository(IDbConnection db)
+  {
+    _db = db;
+  }
+  private readonly IDbConnection _db;
+
+}
---
+++ server/Services/WatchersService.cs
@@ -0,0 +1,14 @@
+
+
+
+namespace postit_dotnet.Services;
+
+public class WatchersService
+{
+  public WatchersService(WatchersRepository repository)
+  {
+    _repository = repository;
+  }
+  private readonly WatchersRepository _repository;
+
+}
